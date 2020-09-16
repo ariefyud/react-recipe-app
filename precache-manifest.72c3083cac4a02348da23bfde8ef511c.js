9e4991d85f3b339f5c9e1ae1f53ad55e1f3f3f43
ariefyud <ariefyud12@gmail.com>
2020-09-16 11:22:05 +0700

    Updates

---
+++ precache-manifest.72c3083cac4a02348da23bfde8ef511c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69a8c10a907b4f93ee3d84fe077e08d",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "8593ddfc9507950b5c14",
+    "url": "/react-recipe-app/static/css/main.9a4e1570.chunk.css"
+  },
+  {
+    "revision": "80329ef14adcff01bc72",
+    "url": "/react-recipe-app/static/js/2.0a75d31b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe-app/static/js/2.0a75d31b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8593ddfc9507950b5c14",
+    "url": "/react-recipe-app/static/js/main.d31bdf63.chunk.js"
+  },
+  {
+    "revision": "71b120ae8903134cbcd0",
+    "url": "/react-recipe-app/static/js/runtime-main.5cac682f.js"
+  }
+]);
